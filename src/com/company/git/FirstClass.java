f3d607ffe834dab2470e11e62ee01a7f862e2a54
zfzf313 <932945838@qq.com>
2020-08-12 11:54:28 +0800

    把本地工程添加到git

--- src/com/company/git/FirstClass.java
+++ src/com/company/git/FirstClass.java
@@ -10,4 +10,10 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git project");
     }
+    public void test(){
+        int i = 0;
+        i +=10;
+
+
+    }
 }
